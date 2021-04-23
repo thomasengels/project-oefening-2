b6b591d0280312a8e51c2f569e6d1892cacfb903
Thomas Engels <thomas.engels92@gmail.com>
2021-04-23 11:24:16 +0200

    initial setup
    -dto module
    -angular module

---
+++ main/src/main/java/com/realdolmen/demo/RealjobsApplication.java
@@ -0,0 +1,13 @@
+package com.realdolmen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealjobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealjobsApplication.class, args);
+    }
+
+}
---
+++ main/src/test/java/com/realdolmen/demo/RealjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realdolmen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealjobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
